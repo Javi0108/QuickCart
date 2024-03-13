ff6f6e1f208bf7fa7f1f25fde30d6051c08ff92c
AlejandroPerMar <aleperzmartin@gmail.com>
2024-03-13 21:19:55 +0100

    Se añade las funcionalidades base de register y login a falta de probar con la aplicacion Angular

--- quickdjango/accounts/views.py
+++ quickdjango/accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_user(username, password=password)
+            return JsonResponse({'message': 'User created successfully'})
+        else:
+            return JsonResponse({'message': 'Username already exists'}, status=400)
+    else:
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
+
+@csrf_exempt
+def user_login(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Login successful'})
+        else:
+            return JsonResponse({'message': 'Invalid credentials'}, status=400)
+    else:
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
