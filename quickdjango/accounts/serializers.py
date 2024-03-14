51cd6fa8b46de50d46b151df844b5847510cfa22
AlejandroPerMar <aleperzmartin@gmail.com>
2024-03-14 17:26:42 +0100

    Pruebas con diferentes endpoints y modos para login y register con User de django.auth

--- quickdjango/accounts/serializers.py
+++ quickdjango/accounts/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from .models import Profile
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password','type']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
