0c72a915686615812f75ce5f714a6a18dd534b44
raulglezm <raulglezmartin@gmail.com>
2024-03-15 02:11:22 +0000

    se arregla el merge

--- quick-ionic/src/app/pages/login/login.module.ts
+++ quick-ionic/src/app/pages/login/login.module.ts
@@ -5,8 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
+
 
 @NgModule({
   imports: [
