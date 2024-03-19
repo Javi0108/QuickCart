aa9763d60dcaf4688ba3540d946c06e5f43d1b42
Raulglezm <Raulglezmartin@gmail.com>
2024-03-19 14:19:15 +0000

    se crean interfaces

--- quick-ionic/src/app/interfaces/profile.interface.ts
+++ quick-ionic/src/app/interfaces/profile.interface.ts
@@ -1,7 +1,29 @@
+import { Shop } from './shop.interface';
 import { User } from './user.interface';
 
 export interface Profile {
   id_profile: number;
   user: User;
   user_type: string;
+}
+
+export interface ProfileSeller {
+  id_profile: number;
+  user: User;
+  user_type: string;
+  phone: string;
+  mobile: string;
+  address: string;
+  socials: ("Instagram" | "Facebook" | "X" | "LinkedIn")[]; // Limitado a Instagram, Facebook y X
+  sales: number;
+  shop: Shop[];
+}
+
+export interface ProfileClient {
+  id_profile: number;
+  user: User;
+  user_type: string;
+  phone: string;
+  mobile: string;
+  address: string;
 }
---
+++ quick-ionic/src/app/interfaces/shop.interface.ts
@@ -0,0 +1,21 @@
+// export interface Shop {
+//     name: string;
+//     title: string;
+//     logoUrl: string;
+//     description: string;
+//     address: string;
+//     products: Product[];
+// }
+
+// export interface Product {
+//     id_product: number;
+//     product_name: string;
+//     price: number;
+//     description: string;
+//     stock_quantity: number;
+// }
+
+export interface Shop{
+    name: string;
+    logoUrl: string;
+}
