8ceacc529b0373b0c9a703fc17b6e1f345d52587
Raulglezm <Raulglezmartin@gmail.com>
2024-03-20 00:10:40 +0000

    se añade la posibilidad de elegir el tipo de cuenta al registrarse

--- quick-ionic/src/app/pages/register/register.page.ts
+++ quick-ionic/src/app/pages/register/register.page.ts
@@ -10,28 +10,52 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterPage implements OnInit {
   pageloaded: boolean;
-
   registerForm: FormGroup;
   passwordError: boolean;
-
 
   constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
     this.pageloaded = false;
     this.passwordError = false;
 
     this.registerForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
+      email: [{ value: '', disabled: true }, [Validators.required, Validators.email]],
+      username: [{ value: '', disabled: true }, Validators.required], // Deshabilitado por defecto
+      password: [{ value: '', disabled: true }, Validators.required], // Deshabilitado por defecto
+      confirmPassword: [{ value: '', disabled: true }, Validators.required], // Deshabilitado por defecto
+      user_type: ['', [Validators.required, this.userTypeValidator]]
     });
+  }
+
+  userTypeValidator(control: any) {
+    if (control.value !== 'Client' && control.value !== 'Seller') {
+      return { 'invalidUserType': true };
+    }
+    return null;
+  }
+
+  onUserTypeChange() {
+    const userType = this.registerForm.get('user_type')?.value;
+    if (userType === 'Client' || userType === 'Seller') {
+      // Si se selecciona un tipo de usuario válido, habilitar los campos correspondientes
+      this.registerForm.get('email')?.enable();
+      this.registerForm.get('username')?.enable();
+      this.registerForm.get('password')?.enable();
+      this.registerForm.get('confirmPassword')?.enable();
+    } else {
+      // Si no se selecciona un tipo de usuario válido, deshabilitar los campos
+      this.registerForm.get('email')?.disable();
+      this.registerForm.get('username')?.disable();
+      this.registerForm.get('password')?.disable();
+      this.registerForm.get('confirmPassword')?.disable();
+    }
   }
 
   register() {
     if (this.registerForm.valid) {
       const user = this.registerForm.value;
+      console.log(user);
       this.authService.register(user).subscribe((data) => {
-        console.log(data)
+        console.log(data);
         this.router.navigate(['/login']);
       });
     }
