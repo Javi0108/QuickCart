3b0f65120ca3bbc702ebafcf931a2c5ac1965a1e
Raulglezm <Raulglezmartin@gmail.com>
2024-04-14 17:47:53 +0100

    se restructura

--- quick-ionic/src/app/components/notification-toast/notification-toast.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { NotificationToastComponent } from "./notification-toast.component";
-
-@NgModule ({
-    declarations: [NotificationToastComponent],
-    exports: [NotificationToastComponent]
-})
-
-export class NotificationToastModule{}
