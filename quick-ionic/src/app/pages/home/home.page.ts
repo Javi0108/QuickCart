be9d9270495040fe7b35a5a5d4ac984ee155de78
Javi0108 <jgarcia@eturia.es>
2024-05-08 01:11:18 +0100

    Mejoras en el CSS de home page

--- quick-ionic/src/app/pages/home/home.page.ts
+++ quick-ionic/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  @ViewChild(IonContent) content!: IonContent;
+  
   constructor() { }
 
   ngOnInit() {
@@ -15,6 +17,18 @@ export class HomePage implements OnInit {
   isLoggedIn(): boolean {
     const token = localStorage.getItem('token');
     return token !== null;
+  }
+
+  scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const yOffset = element.offsetTop;
+      this.content.scrollToPoint(0, yOffset, 1000);
+    }
+  }
+
+  stopPropagation(event: Event): void {
+    event.stopPropagation();
   }
 
 }
